9eda485e8be24eca127ae807ed8f2e64fb916dcc
Offado <161487322+Offado@users.noreply.github.com>
2025-08-15 11:25:47 +0000

    FullStack_JS

--- backend/app.js
+++ backend/app.js
@@ -50,6 +50,19 @@ app.use("/api/stuff", (req, res, next) => {
       price: 2900,
       userId: "qsomihvqios",
     },
+    {
+      _id: "1755172641608",
+      title: "Wireframes UX",
+      description:
+        "Pages prêtes à l'emploi : Connexion, Inscription, Tableau de bord, etc.\n" +
+        "Fichiers : .fig (Figma)\n" +
+        "Versions : Mobile & Web\n" +
+        "Bonus Pro : CheckList et notion ux/ui, PDF",
+      imageUrl:
+        "https://view.subpage.app/app/company/C532b8873cc5442e2b1f2265b77a7d7dc/domain/MTiT0jFlGh/page/M6K4TirmGh/glossary/M88cc1363aa97e7307deaa1b41bf3be481676987770609/file/M975bde55df2de8b71918cd315e7c8c431708091745175.png",
+      price: 2500,
+      userId: "userID40282382",
+    },
   ];
   res.status(200).json(stuff);
 });
@@ -59,14 +72,63 @@ app.use("/api/stuff", (req, res, next) => {
 
 // On importe mongoose d'abord
 // Mongoose permet de définir des schémas pour tes données et de gérer les opérations (CRUD) plus facilement.
-const mongoose = require('mongoose');
-// sert à gérer les variables d’environnement dans un projet, généralement pour éviter de mettre des informations 
+const mongoose = require("mongoose");
+
+// dotenv sert à gérer les variables d’environnement dans un projet, généralement pour éviter de mettre des informations
 // sensibles directement dans le code (comme des mots de passe, clés API, URL de base de données, etc.).
-require('dotenv').config();
+require("dotenv").config();
 
 // Sert à se connecter à une base de données MongoDB.
-mongoose.connect(process.env.MONGO_URI)  // process.env est un objet global dans Node.js qui contient toutes les variables d’environnement disponibles pour ton application.
-.then(() => console.log('Connexion à MongoDB réussie !'))     // Le rappel à exécuter lorsque la promesse est résolue.
-.catch((err) => console.log('Connexion à MongoDB échouée !', err));   // Le rappel à exécuter en cas de rejet de la promesse.
+mongoose
+  .connect(process.env.MONGO_URI) // process.env est un objet global dans Node.js qui contient toutes les variables d’environnement disponibles pour ton application.
+  .then(() => console.log("Connexion à MongoDB réussie !")) // Le rappel à exécuter lorsque la promesse est résolue.
+  .catch((err) => console.log("Connexion à MongoDB échouée !", err)); // Le rappel à exécuter en cas de rejet de la promesse.
+
+// Création d'une instance de model
+const Thing = require("./models/thing");
+
+// Enregistrez les données
+app.post("/api/stuff", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body, // copie de tous les éléments de req.body
+  });
+  thing
+    .save() // enregistre toutes les informations dans le base de données
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// Mise à jour des données
+app.put('/api/stuff/:id', (req, res, next) => {
+  // Cela nous permet de mettre à jour le Thing
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// Supprimer les données
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// Récupérer les données par id
+app.get("/api/stuff/:id", (req, res, next) => {
+  // retourne un seul Thing basé sur la fonction de comparaison qu'on lui passe (souvent pour récupérer un Thing par son identifiant unique).
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+// Récupérer les données
+app.get("/api/stuff", (req, res, next) => {
+  // retourne tous les Things
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
+});
+
 
 module.exports = app;
---
+++ backend/models/thing.js
@@ -0,0 +1,18 @@
+// On importe mongoose
+const mongoose = require("mongoose");
+
+// nous créons un schéma de données qui contient les champs souhaités pour chaque Thing
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+// La méthode  Schema  de Mongoose nous permet de créer un schéma de données pour notre base de données MongoDB.
+
+// La méthode  model  transforme ce modèle en un modèle utilisable.
+module.exports = mongoose.model('Thing', thingSchema);
+
+
+
